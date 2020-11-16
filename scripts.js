0773c07f8cd7d80e436b45d8a11691453814f68b
Vibhu <vibhu98mishra@gmail.com>
2020-11-16 16:59:47 +0530

    Background Sync and Notification feature :sparkles:

--- scripts.js
+++ scripts.js
@@ -98,3 +98,25 @@ function installApp() {
 window.addEventListener("appinstalled", evt => {
 	console.log("appinstalled fired", evt);
 });
+
+
+// ********* Background Sync *************
+
+// Get Permissions
+function registerNotification() {
+	Notification.requestPermission(permission => {
+		if (permission === 'granted'){ registerBackgroundSync() }
+		else console.error("Permission was not granted.")
+	})
+}
+
+function registerBackgroundSync() {
+    if (!navigator.serviceWorker){
+        return console.error("Service Worker not supported")
+    }
+
+    navigator.serviceWorker.ready
+    .then(registration => registration.sync.register('syncAttendees'))
+    .then(() => console.log("Registered background sync"))
+    .catch(err => console.error("Error registering background sync", err))
+}
--- sw.js
+++ sw.js
@@ -88,3 +88,20 @@ self.addEventListener("fetch", event => {
     );
   }
 });
+
+
+// sync event 
+self.addEventListener('sync', function(event) {
+	console.log("sync event", event);
+    if (event.tag === 'syncAttendees') {
+      event.waitUntil(syncAttendees()); // sending sync request
+    }
+});
+
+function syncAttendees(){
+	return update({ url: `https://reqres.in/api/users` })
+    	.then(refresh)
+    	.then((attendees) => self.registration.showNotification(
+    		`${attendees.length} attendees to the PWA Workshop`
+    	))
+}
